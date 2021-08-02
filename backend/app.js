4f8cf05cb0cfc0b970d95e137cde82f230f8879c
진지연 <serious_yeon@naver.com>
2021-08-02 16:54:57 +0900

    0802-recom-jy

--- backend/app.js
+++ backend/app.js
@@ -7,16 +7,16 @@ const session = require('express-session');
 // const FileStore = require('session-file-store')(session);
 const path = require('path');
 const cors = require('cors');     // Django 서버와 통신하기 위함
-const { passport } = require('./utils/auth');
+// const { passport } = require('./utils/auth');
 // dotenv
 dotenv.config({
   path:path.join(__dirname, '.env')
 });
 
 
-
 // 앱 실행
-const testRouter = require('./routes/test');
+const ingredientRouter = require('./routes/ingredient');
+// const testRouter = require('./routes/test');
 // const userRouter = require('./routes/user');
 const recipeRouter = require('./routes/recipe');
 const searchRouter = require('./routes/search');
@@ -34,9 +34,15 @@ app.use(cors({
 	credentials: true,
 }));                                // Django 서버와 통신하기 위함
 
+// 배포할 때 dotenv 수정해주기!
+app.use((req, res, next) => {
+  global.django_origin = process.env.DJANGO_ORIGIN||'http://localhost:8000'
+  console.log(django_origin)
+  next()
+})
 
 // 라우터
-app.use('/test', testRouter);
+app.use('/ingredient', ingredientRouter)
 // app.use('/user', userRouter);
 app.use('/recipe', recipeRouter);
 app.use('/search', searchRouter);
