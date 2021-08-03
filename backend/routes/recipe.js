5db099fef871666044cd0ef1a8cb65f597e4c9a9
MinsangPark <whsrpdla@naver.com>
2021-08-03 16:28:57 +0900

    bigpatch

--- backend/routes/recipe.js
+++ backend/routes/recipe.js
@@ -7,16 +7,162 @@ const axios = require('axios');
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
-// // 배포할 때 여기나 dotenv 수정해주기!
-// router.use((req, res, next) => {
-//     global.django_origin = 'http://localhost:8000'
-//     if (process.env.DJANGO_ORIGIN) {
-//         global.django_origin = process.env.DJANGO_ORIGIN
-//         console.log('django_origin을 글로벌변수로 지정했습니다')
-//     }
-//     next()
-// })
+// 추천로직
+router.get('/recom/retaining', (req, res, next) => {
+    const url = django_origin + '/recipe/recom/retaining/'
+    axios.get(url)
+    .then(response => { 
+        return response.data.similar_recipe_id
+    })
+    .then(response => { 
+        db.query(`SELECT * FROM recipe_infos WHERE recipe_info_id IN (${response[0]}, ${response[1]}, ${response[2]})`, (err, rows, fields) => {
+            if (err) next (err)
+            res.send(rows)
+        })
+    })
+    .catch(error => { next(error) })
+});
 
+// 추천로직
+router.get('/recom/expire', (req, res, next) => {
+    const url = django_origin + '/recipe/recom/expire/'
+    axios.get(url)
+    .then(response => { 
+        console.log(response.data.similar_recipe_id)
+        return response.data.similar_recipe_id
+    })
+    .then(response => {
+        db.query(`SELECT * FROM recipe_infos WHERE recipe_info_id IN (${response[0]}, ${response[1]}, ${response[2]})`, (err, rows, fields) => {
+            if (err) next (err)
+            res.send(rows)
+        })
+    })
+    .catch(error => { next(error) })  
+})
+
+
+// title search
+router.post('/search/title/:searchWord', (req, res, next) => {
+    let searchWord = req.params.searchWord;
+    let limit = 3; // limit 값 논의 필요
+
+    let beforeId = req.body.before_recipe_info_id;
+    let beforeView = req.body.before_view;
+
+    let title_promise;
+    //첫번째 요청
+    if (beforeId===undefined || beforeView===undefined) {
+        title_promise = new Promise((resolve, reject) => {
+            db.query(`select * 
+                from recipe_infos
+                where match(title) against('${searchWord}*' in boolean mode)
+                order by view desc, recipe_info_id desc
+                limit ${limit}`, (err, rows) => {
+                if (err) {
+                    console.log(err);
+                    reject(err);
+                }
+                resolve(rows);
+            })
+        })
+    }
+    //추가 요청
+    else {
+        title_promise = new Promise((resolve, reject) => {
+            db.query(`select * from
+                (select *
+                from recipe_infos
+                where match(title) against ('${searchWord}*' in boolean mode)
+                    and
+                    (
+                        (view = ${beforeView} and recipe_info_id<${beforeId})
+                        or
+                        (view < ${beforeView})
+                    )
+                ) tmp
+                order by view desc, recipe_info_id desc
+                limit ${limit};
+                `, (err, rows) => {
+                if (err) reject(err);
+                resolve(rows);
+            })
+        })
+    }
+
+    title_promise
+        .then((rows) => {
+            res.send(rows);
+        })
+        .catch((err) => {
+            next(err);
+        })
+})
+
+// ingredient search
+router.post('/search/ingredient/:searchWord', (req, res, next) => {
+    let searchWord = req.params.searchWord;
+    let limit = 3; // limit 값 논의 필요
+
+    let beforeId = req.body.before_recipe_info_id;
+    let beforeView = req.body.before_view;
+
+
+    let ingredient_promise;
+    //첫 번째 요청
+    if (beforeId === undefined || beforeView === undefined) {
+        ingredient_promise = new Promise((resolve, reject) => {
+            db.query(`
+                select *
+                from recipe_infos
+                where recipe_info_id in
+                    (select recipe_info_id
+                    from ingredients_and_recipe_infos
+                    where match(ingredient_name) against('${searchWord}*' in boolean mode)
+                    group by recipe_info_id)
+                order by view desc, recipe_info_id desc
+                limit ${limit}`, (err, rows) => {
+                if (err) {
+                    console.log(err);
+                    reject(err);
+                }
+                resolve(rows);
+            })
+        })
+    }
+    //추가 요청
+    else {
+        ingredient_promise = new Promise((resolve, reject) => {
+            db.query(
+                `
+                select *
+                from recipe_infos
+                where recipe_info_id in
+                (
+                    select recipe_info_id
+                    from ingredients_and_recipe_infos
+                    where match(ingredient_name) against('${searchWord}*' in boolean mode)
+                    group by recipe_info_id
+                )
+                and ((view=${beforeView} and recipe_info_id != ${beforeId}) or (view<${beforeView}))
+                order by view desc, recipe_info_id desc
+                limit ${limit}
+                `
+                , (err, rows) => {
+                if (err) reject(err)
+                resolve(rows);
+            })
+        })
+    }
+
+    ingredient_promise
+        .then((rows) => {
+            res.send(rows);
+        })
+        .catch((err) => {
+            next(err);
+        })
+    
+})
 
 // 라우터
 router.get('/:recipeId', (req, res) => {
@@ -66,45 +212,6 @@ router.get('/:recipeId', (req, res) => {
         next(err);
     })
 })
-
-
-// 추천로직
-router.get('/recom/retaining', (req, res, next) => {
-    const url = django_origin + '/recipe/recom/retaining/'
-    axios.get(url)
-    .then(response => { 
-        return response.data.similar_recipe_id
-    })
-    .then(response => { 
-        db.query(`SELECT * FROM recipe_infos WHERE recipe_info_id IN (${response[0]}, ${response[1]}, ${response[2]})`, (err, rows, fields) => {
-            if (err) next (err)
-            res.send(rows)
-        })
-    })
-    .catch(error => { next(error) })
-});
-
-// 추천로직
-router.get('/recom/expire', (req, res, next) => {
-    const url = django_origin + '/recipe/recom/expire/'
-    axios.get(url)
-    .then(response => { 
-        console.log(response.data.similar_recipe_id)
-        return response.data.similar_recipe_id
-    })
-    .then(response => {
-        db.query(`SELECT * FROM recipe_infos WHERE recipe_info_id IN (${response[0]}, ${response[1]}, ${response[2]})`, (err, rows, fields) => {
-            if (err) next (err)
-            res.send(rows)
-        })
-    })
-    .catch(error => { next(error) })  
-})
-
-
-
-
-
 
 
 //404 middleware
