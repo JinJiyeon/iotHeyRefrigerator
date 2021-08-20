7d1e7a4da49b3d019b730f8e15fb991f00e30cc4
차성민 <ckpow@naver.com>
2021-08-20 11:37:24 +0900

    코드 최종본

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const db = mysql.createConnection({ //인자로 객체 전달
     host:process.env.DB_HOST,
     user:process.env.DB_USERNAME,
-    password:'fldclfqorqjsgh',
+    password:process.env.DB_PASSWORD,
     database:process.env.DB_DATABASE,  
     dateStrings : 'date'
 });
